7aa5c65e7c4592d2cdc5350e25c75cc22f2d62df
resinner <revengingsinner@gmail.com>
2023-08-29 17:01:08 +0300

    Data and computed

--- my-first-app/src/App.vue
+++ my-first-app/src/App.vue
@@ -1,21 +1,40 @@
 <template>
   <div id="app">
 
-<Button>Click me</Button>
+    <h1>{{ title }}</h1>
+<Button @click="increment">Click me</Button>
 
   </div>
 </template>
 
+
 <script>
+
 import Button from './components/Button.vue'
 
 export default {
   name: 'App',
   components: {
 Button
+  },
+  data() {
+    return {
+      amountOfClick: 0
+    }
+  },
+  computed: {
+    title() {
+      return `Amount of clicks ${this.amountOfClick}`
+    }
+  },
+  methods: {
+    increment() {
+      this.amountOfClick += 1
+    }
   }
 }
 </script>
+
 
 <style>
 #app {
