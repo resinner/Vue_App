5fed9ba55e51d897bef4af251730053e07b0e4b6
resinner <revengingsinner@gmail.com>
2023-08-30 16:24:33 +0300

    rewrite with slots

--- my-first-app/src/App.vue
+++ my-first-app/src/App.vue
@@ -1,45 +1,34 @@
 <template>
   <div :id="$style.app">
-    <!-- <ApartmentsItem 
-    :descr="apartment.descr"
-    :price="apartment.price"
-    :rating="apartment.rating"
-    imgSrc="https://photoresources.wtatennis.com/photo-resources/2020/07/02/87020751-8eee-44fa-8105-f4057c1be863/Muchova_Hero-Smile.png?width=950"
-
-    /> -->
-
-    <ApartmentsList :items="apartments"/>
+    <ApartmentsList :items="apartments">
+      <template v-slot:apartment="{ apartment }">
+        <ApartmentsItem
+          :key="apartment.id"
+          :descr="apartment.descr"
+          :rating="apartment.rating"
+          :imgSrc="apartment.imgUrl"
+          :price="apartment.price"
+        />
+      </template>
+    </ApartmentsList>
   </div>
 </template>
 
 <script>
 import ApartmentsList from "./components/apartment/ApartmentsList";
-import apartments from './components/apartment/apartments'
+import ApartmentsItem from "./components/apartment/ApartmentsItem.vue";
+import apartments from "./components/apartment/apartments";
 
 export default {
   name: "App",
   components: {
     ApartmentsList,
+    ApartmentsItem,
   },
   data() {
     return {
-      apartments,
-      apartment: {
-        id: "5f05c9dad2c9bc0f773444bc",
-        title: "Aut qui adipisci discinctio maiores molesstiae sit est inventore vero.",
-        descr: "Роскошные апартаменты в скайндинавском стиле с панорамным видом на реку и бесплатным Wi-Fi.",
-        price: 1250,
-        rating: 4.5,
-        location: {
-          city: "Kherson",
-        },
-        owner: {
-          name: "Ellen",
-          phone: "115-355-5652",
-          email: "Tracey.Morar86@hotmail.com",
-        },
-      },
-    };
+      apartments
+    }
   },
 };
 </script>
--- my-first-app/src/components/apartment/ApartmentsList.vue
+++ my-first-app/src/components/apartment/ApartmentsList.vue
@@ -1,48 +1,36 @@
 <template>
-    <Container>
+  <Container>
+    <slot name="title"></slot>
     <div class="apartments-list">
-            <ApartmentsItem
-            v-for="{ id, descr, rating, imgUrl, price} in items"
-            :key="id"
-            :descr="descr"
-            :rating="rating"
-            :imgSrc="imgUrl"
-            :price="price"
-            class="apartments-list__item"
-            />
-        </div>
-    </Container>
+      <template v-for="apartment in items">
+        <slot name="apartment" v-bind:apartment="apartment"></slot>
+      </template>
+    </div>
+  </Container>
 </template>
 
 <script>
+import Container from "../shared/Container.vue";
 
-import ApartmentsItem from './ApartmentsItem.vue';
-import Container from '../shared/Container.vue'
-
-    export default {
-    name: 'ApartmentsList',
-    components: {
-        ApartmentsItem,
-        Container
+export default {
+  name: "ApartmentsList",
+  components: {
+    Container,
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
     },
-    props: {
-        items: {
-            type: Array,
-                default: () => []
-            }
-        }
-    }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .apartments-list {
-    display: flex;
-    flex-wrap: wrap;
-    margin-left: -15px;
-    margin-right: -15px;
-
-    &__item {
-        margin-bottom: 30px;
-    }
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -15px;
+  margin-right: -15px;
 }
 </style>
