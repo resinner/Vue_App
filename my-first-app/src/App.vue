036159d2088b21466be8660fa24c637a3ea27c64
resinner <revengingsinner@gmail.com>
2023-08-30 15:32:57 +0300

    add apartment-image and shared-conteiner

--- my-first-app/src/App.vue
+++ my-first-app/src/App.vue
@@ -1,25 +1,29 @@
 <template>
   <div :id="$style.app">
-    <ApartmentsItem 
+    <!-- <ApartmentsItem 
     :descr="apartment.descr"
     :price="apartment.price"
     :rating="apartment.rating"
     imgSrc="https://photoresources.wtatennis.com/photo-resources/2020/07/02/87020751-8eee-44fa-8105-f4057c1be863/Muchova_Hero-Smile.png?width=950"
 
-    />
+    /> -->
+
+    <ApartmentsList :items="apartments"/>
   </div>
 </template>
 
 <script>
-import ApartmentsItem from "./components/apartment/ApartmentsItem";
+import ApartmentsList from "./components/apartment/ApartmentsList";
+import apartments from './components/apartment/apartments'
 
 export default {
   name: "App",
   components: {
-    ApartmentsItem,
+    ApartmentsList,
   },
   data() {
     return {
+      apartments,
       apartment: {
         id: "5f05c9dad2c9bc0f773444bc",
         title: "Aut qui adipisci discinctio maiores molesstiae sit est inventore vero.",
---
+++ my-first-app/src/components/apartment/ApartmentsList.vue
@@ -0,0 +1,48 @@
+<template>
+    <Container>
+    <div class="apartments-list">
+            <ApartmentsItem
+            v-for="{ id, descr, rating, imgUrl, price} in items"
+            :key="id"
+            :descr="descr"
+            :rating="rating"
+            :imgSrc="imgUrl"
+            :price="price"
+            class="apartments-list__item"
+            />
+        </div>
+    </Container>
+</template>
+
+<script>
+
+import ApartmentsItem from './ApartmentsItem.vue';
+import Container from '../shared/Container.vue'
+
+    export default {
+    name: 'ApartmentsList',
+    components: {
+        ApartmentsItem,
+        Container
+    },
+    props: {
+        items: {
+            type: Array,
+                default: () => []
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.apartments-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: -15px;
+    margin-right: -15px;
+
+    &__item {
+        margin-bottom: 30px;
+    }
+}
+</style>
