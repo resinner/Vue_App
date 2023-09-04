3f82d9b9cbbac80d3e53b357e6a6042d8535cd5d
resinner <revengingsinner@gmail.com>
2023-09-04 22:09:15 +0300

    Add all component to reviews column

---
+++ my-first-app/src/components/reviews/reviews-item/ReviewsHeading.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="reviews-heading">
+    <Avatar class="reviews-heading__avatar"/>
+    <div class="reviews-heading__content">
+      <span class="reviews-heading__author">{{ author }}</span>
+      <StarRating :rating="rating" />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import StarRating from "@/components/StarRating.vue";
+import Avatar from "./ReviewsAvatar.vue";
+
+export default {
+  name: "ReviewsHeading",
+  components: {
+    StarRating,
+    Avatar,
+  },
+  props: {
+    author: {
+      type: String,
+      required: true,
+    },
+    reting: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.reviews-heading {
+  display: flex;
+  align-items: center;
+
+  &__avatar {
+    margin-right: 18px;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
--- my-first-app/src/pages/Apartment.vue
+++ my-first-app/src/pages/Apartment.vue
@@ -3,10 +3,13 @@
     <Container>
       <div class="apartment-page__content">
         <ApartmentsMainInfo :apartment="apartment" />
-        <ApartmentsOwner
-          class="apartment-page__owner"
-          :owner="apartment.owner"
-        />
+        <div class="apartment-page__additional-info">
+          <ApartmentsOwner
+            class="apartment-page__owner"
+            :owner="apartment.owner"
+          />
+<Reviews :reviews="reviewsList"/>
+        </div>
       </div>
     </Container>
   </main>
@@ -17,6 +20,8 @@ import Container from "../components/shared/Container";
 import apartments from "../components/apartment/apartments";
 import ApartmentsMainInfo from "../components/apartment/ApartmentsMainInfo";
 import ApartmentsOwner from "../components/apartment/ApartmentsOwner";
+import Reviews from '../components/reviews'
+import reviewsList from '../components/reviews/reviews.json'
 
 export default {
   name: "ApartmentPage",
@@ -24,8 +29,12 @@ export default {
     Container,
     ApartmentsMainInfo,
     ApartmentsOwner,
+    Reviews
   },
   computed: {
+    reviewsList() {
+return reviewsList
+    },
     apartment() {
       return apartments.find(
         (apartment) => apartment.id === this.$route.params.id
