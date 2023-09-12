acb0d85c9cf895d0ec7a17c61959c03c872d316a
resinner <revengingsinner@gmail.com>
2023-09-12 19:07:13 +0300

    create RegistrationPage

---
+++ my-first-app/src/components/auth/registration/index.vue
@@ -0,0 +1,146 @@
+<template>
+  <AuthContainer class="registration">
+    <MainTitle class="registration__title">Логін</MainTitle>
+    <Form ref="form" class="registration__form" @submit.prevent="handleSubmit">
+      <CustomInput
+        v-model="formData.name"
+        placeholder="Name"
+        autocomplete="username"
+        name="name"
+        :rules="nameRules"
+        class="registration__input"
+      />
+      <CustomInput
+        v-model="formData.email"
+        placeholder="Email"
+        autocomplete="email"
+        name="email"
+        :rules="emailRules"
+        class="registration__input"
+      />
+      <CustomInput
+        v-model="formData.password"
+        placeholder="Password"
+        autocomplete="current-password"
+        type="password"
+        name="password"
+        :rules="passwordRules"
+        class="registration__input"
+      />
+      <CustomInput
+        v-model="formData.confirmPassword"
+        placeholder="Confirm password"
+        autocomplete="current-password"
+        type="password"
+        name="password"
+        :rules="comfirmPassword"
+        class="registration__input"
+      />
+      <Button class="registration__btn" type="submit" :loading="loading"
+        >Вхід</Button
+      >
+    </Form>
+  </AuthContainer>
+</template>
+
+<script>
+import Form from "../../shared/form";
+import CustomInput from "../../shared/CustomInput";
+import Button from "../../shared/Button";
+import AuthContainer from "../../auth/AuthContainer";
+import MainTitle from "../../shared/MainTitle";
+import {
+  emailValidation,
+  passwordValidation,
+  isRequired,
+} from "../../../utils/validationRules";
+import { registerUser } from "../../../services/auth.service";
+
+export default {
+  name: "Registration",
+  components: {
+    Form,
+    CustomInput,
+    Button,
+    AuthContainer,
+    MainTitle,
+  },
+  data() {
+    return {
+      loading: false,
+      formData: {
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+    };
+  },
+  computed: {
+    rules() {
+      return {
+        emailValidation,
+        passwordValidation,
+        isRequired,
+      };
+    },
+    nameRules() {
+      return [this.rules.isRequired];
+    },
+    emailRules() {
+      return [this.rules.isRequired, this.rules.emailValidation];
+    },
+    passwordRules() {
+      return [this.rules.isRequired, this.rules.passwordValidation];
+    },
+    comfirmPassword() {
+      return [
+        (val) => ({
+          hasPassed: val === this.formData.password,
+          message: "Пароль не співпадає",
+        }),
+      ];
+    },
+  },
+  methods: {
+    async handleSubmit() {
+      const { form } = this.$refs;
+      const isFormValid = form.validate();
+      const { name, password, email } = this.formData;
+
+      if (isFormValid) {
+        try {
+          const { data } = await registerUser({ name, password, email });
+          console.log(data);
+          form.reset();
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.registration {
+  &__form {
+    display: block;
+    flex-direction: column;
+  }
+
+  &__title {
+    text-align: center;
+  }
+
+  &__input {
+    margin-bottom: 20px;
+    width: 100%;
+  }
+
+  &__btn {
+    margin-top: 15px;
+    width: 100%;
+  }
+}
+</style>
--- my-first-app/src/services/auth.service.js
+++ my-first-app/src/services/auth.service.js
@@ -3,3 +3,8 @@ import axios from "../utils/axios";
 export const loginUser = (payload) => {
   return axios.post("/users/login", payload);
 };
+
+export const registerUser = (payload) => {
+  return axios.post("/users/register", payload);
+};
+
