0ffd30d03eb14e0ccba97491d3fb041ee6301153
resinner <revengingsinner@gmail.com>
2023-09-15 19:43:01 +0300

    add bookApartment service

--- my-first-app/src/components/apartment/ApartmentsMainInfo.vue
+++ my-first-app/src/components/apartment/ApartmentsMainInfo.vue
@@ -1,28 +1,66 @@
 <template>
   <article class="apartment-main-info">
     <div class="apartment-main-info__heading">
-        <h1 class="apartment-main-info__title">{{ apartment.title }}</h1>
-        <StarRating :rating="apartment.rating"/>
+      <h1 class="apartment-main-info__title">{{ apartment.title }}</h1>
+      <StarRating :rating="apartment.rating" />
     </div>
     <img :src="apartment.imgUrl" alt="" class="apartment-main-info__photo" />
     <p class="apartment-main-info__description">{{ apartment.descr }}</p>
+    <div class="apartment-main-info__btn">
+      <Button @click="handleApartmentsBooking" :loading="isLoading">
+        Забронювати
+      </Button>
+    </div>
   </article>
 </template>
 
 <script>
-
-import StarRating from '../StarRating.vue';
+import StarRating from "../StarRating.vue";
+import Button from "../shared/Button.vue";
+import { bookApartment } from "../../services/order.service";
 
 export default {
-    name: "ApartmentsMainInfo",
-    components: {
-    StarRating
+  name: "ApartmentsMainInfo",
+  components: {
+    StarRating,
+    Button,
   },
-    props: {
-        apartment: {    
-            type: Object,
-            require: true,
-    }
+  props: {
+    apartment: {
+      type: Object,
+      require: true,
+    },
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
+  methods: {
+    async handleApartmentsBooking() {
+      const body = {
+        apartmentId: this.$route.params.id,
+        date: Date.now(),
+      };
+
+      try {
+        this.isLoading = true;
+        await bookApartment(body);
+
+        this.$notify({
+          type: "success",
+          title: "Замовлення додано",
+        });
+      } catch (error) {
+        this.$notify({
+          type: "error",
+          title: "Виникла помилка",
+          text: error.message,
+        });
+      } finally {
+        this.isLoading = false;
+      }
+    },
   },
 };
 </script>
