33e4f5704628c8d9138c71d9a54a1ab316e5d81b
resinner <revengingsinner@gmail.com>
2023-09-18 23:07:57 +0300

    delete console log

---
+++ src/pages/ErrorPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="error-container">
+    <section class="error">
+      <h1 class="error__title">Такої сторінки не існує!</h1>
+    </section>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "ErrorPage",
+};
+</script>
+
+
+<style lang="scss" scoped>
+.error-container {
+  position: relative;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  background: url("../assets/img/custom-error-pages.png") no-repeat;
+  background-size: cover;
+  background-position: center;
+  min-height: calc(100vh - 30px);
+  z-index: 1;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(15, 29, 45, 0.5);
+    z-index: -1;
+  }
+}
+.error {
+  margin-top: 80px;
+
+  &__title {
+    font-size: 60px;
+    text-align: center;
+    color: #fff;
+  }
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,80 @@
+import VueRouter from "vue-router";
+import ErrorPage from "./pages/ErrorPage";
+import store from "./store";
+
+const HomePage = () => import("./pages/HomePage");
+const ApartmentPage = () => import("./pages/Apartment");
+const LoginPage = () => import("./pages/Login");
+const RegistrationPage = () => import("./pages/Registration");
+const MyOrdersPage = () => import("./pages/MyOrders");
+
+
+const routes = [
+  {
+    path: "/",
+    component: HomePage,
+    name: "homepage",
+  },
+  {
+    path: "/apartments/:id",
+    component: ApartmentPage,
+    name: "apartment",
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/my-orders",
+    component: MyOrdersPage,
+    name: "my-orders",
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    component: LoginPage,
+    name: "login-page",
+    meta: {
+      hideForAuth: true,
+    },
+  },
+  {
+    path: "/registration",
+    component: RegistrationPage,
+    name: "registration-page",
+    meta: {
+      hideForAuth: true,
+    },
+  },
+  {
+    path: "*",
+    component: ErrorPage,
+    name: "error-page",
+  },
+];
+
+const router = new VueRouter({
+  routes,
+  mode: "history",
+});
+
+router.beforeEach((to, from, next) => {
+  const isLoggedIn = store.getters["auth/isLoggedIn"];
+
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!isLoggedIn) {
+      next({ name: "login-page" });
+    }
+  }
+
+  if (to.matched.some((record) => record.meta.hideForAuth)) {
+    if (isLoggedIn) {
+      next({ name: "homepage" });
+    }
+  }
+
+  next();
+});
+
+export default router;
