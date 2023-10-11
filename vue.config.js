59e5efdb02dd65d991902f53b25fcc453d11474e
resinner <revengingsinner@gmail.com>
2023-10-11 17:57:51 +0300

    add gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,17 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  publicPath: `/Vue_App/`,
+  transpileDependencies: true,
+});
 
 // module.exports = {
 //   publicPath: "/",
 //   indexPath: "dist/dist/index.html",
 // };
 
-
-module.exports = {
-  productionSourceMap: false,
-  outputDir: "dist",
-  assetsDir: "static",
-  indexPath: "templates/index.html",
-};
+// module.exports = {
+//   productionSourceMap: false,
+//   outputDir: "dist",
+//   assetsDir: "static",
+//   indexPath: "templates/index.html",
+// };
